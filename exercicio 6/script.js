6c44a48f701747a8d69187e41be38f7100be58eb
Lucas Thomaz Brandel Zequini <lucastbzequini@gmail.com>
2026-03-30 00:17:02 -0300

    exercicio 6

---
+++ exercicio 6/script.js
@@ -0,0 +1,99 @@
+const cria = document.getElementById("mainImage");
+const btn = document.getElementById("btnImage");
+const fundoDia = "sabri_bg.png";
+const fundoNoite = "sabri_night.png";
+
+
+const estados = {
+    normal:  "sabri_neutral.png",
+    puto: "sabri_cry.png",
+    morto: "sabri_dead.png",
+    comendo: "sabri_eat.png",
+    alimentado: "sabri_full.png",
+    dormir: "sabri_sleep.png",
+    ferlini: "ferlini.jpg"
+}
+
+let contador = 0; 
+let intervalo = null;
+let time_click = null;
+let time_out = null;
+let dead = 0;
+let horas = 0;
+
+function controlador (){
+    if(intervalo) clearInterval(intervalo)
+        
+        intervalo = setInterval(() => {
+            contador++;
+
+            console.log("tempo:",contador);
+            
+            if (contador == 5){
+                cria.src = estados.puto;
+            }
+
+            if(contador == 10){
+                cria.src = estados.morto;
+                dead++;
+            }
+        }, 1000);
+}
+
+function alimentar() {
+    if (dead > 0) {
+        console.log("Tarde demais... ela se foi.");
+        return;
+    }
+    cria.src = estados.comendo;
+    contador = 0;
+
+    console.log("Comendo");
+
+    if (time_click) clearTimeout(time_click);
+
+    time_click = setTimeout(() => {
+        cria.src = estados.alimentado;
+
+        time_out = setTimeout(() => {
+            cria.src = estados.normal;
+        }, 2000);
+
+    }, 1000);
+}
+
+function atualizarFundo() {
+    if (horas) clearInterval(horas);
+
+    horas = setInterval(() => {
+    horas++;
+    
+    if (horas >= 12) {
+        document.body.style.backgroundImage = `url('${fundoNoite}')`;
+    } else {
+        document.body.style.backgroundImage = `url('${fundoDia}')`;
+    }
+    if(horas >=24) horas =0;
+
+    }, 1000);
+}
+function ferlini() {
+    cria.src = estados.ferlini;
+    contador = 0;
+
+    console.log("GOSTOSINHO");
+
+    if (time_click) clearTimeout(time_click);
+
+    time_click = setTimeout(() => {
+        cria.src = estados.ferlini;
+
+        time_out = setTimeout(() => {
+            cria.src = estados.normal;
+        }, 2000);
+
+    }, 1000);
+}
+
+controlador();
+atualizarFundo();
